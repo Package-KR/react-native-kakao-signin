2ea2b78308cd5336a1904b50917101b76063e17c
dijeungi <dijeungi@naver.com>
2026-04-13 02:28:38 +0900

    ci: release 버전 파일 자동 커밋 설정 추가

--- release.config.js
+++ release.config.js
@@ -15,5 +15,13 @@ module.exports = {
     ],
     '@semantic-release/github',
     '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
